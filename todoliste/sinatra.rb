9c137d26fec33fec78af86400ca930175073ca74
veracl <veraclemens7@googlemail.com>
2016-05-31 20:01:43 +0200

    Added project "todoliste"

---
+++ todoliste/sinatra.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+todos = ["Einkaufen", "Putzen"]
+done = []
+
+get '/' do
+	erb :index, :locals => {:todos => todos, :done => done}
+end
+
+post '/delete' do
+	todo = params[:todo]
+	todos.delete(todo)
+	done.push(todo)
+	redirect to('/')
+end
+
+post '/new' do
+	newtodo = params[:newtodo]
+	if !(newtodo.length == 0)
+		todos.push(newtodo)
+	end
+	redirect to('/')
+end
+
+get '/empty' do
+	done = []
+	redirect to('/')
+end
