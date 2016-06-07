fce37972847f8b82202497268c043698f3e97103
veracl <veraclemens7@googlemail.com>
2016-06-07 20:20:33 +0200

    Switched from arrays to sqlite database
    + added .gitignore for the database file

--- todoliste/sinatra.rb
+++ todoliste/sinatra.rb
@@ -1,28 +1,51 @@
 require 'sinatra'
+require 'active_record'
+require 'sqlite3'
 
-todos = ["Einkaufen", "Putzen"]
-done = []
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database =>  'db.sqlite3.db'
+)
+
+class ToDo < ActiveRecord::Base
+end
+
+class Done < ActiveRecord::Base
+end
+
+#ActiveRecord::Migration.create_table :to_dos do |t|
+#  t.string :name
+#end
+
+#ActiveRecord::Migration.create_table :dones do |t|
+#  t.string :name
+#end
+
+#ToDo.create(name: "Putzen")
 
 get '/' do
-  haml :index, :locals => {:todos => todos, :done => done}
+  @to_dos = ToDo.all()
+  @dones = Done.all()
+  haml :index
 end
 
 post '/delete' do
   todo = params[:todo]
-  todos.delete(todo)
-  done.push(todo)
+  del = ToDo.find_by(name: todo)
+  del.destroy
+  Done.create(name: todo)
   redirect to('/')
 end
 
 post '/new' do
   newtodo = params[:newtodo]
   if !(newtodo.length == 0)
-    todos.push(newtodo)
+    ToDo.create(name: newtodo)
   end
   redirect to('/')
 end
 
 get '/empty' do
-  done = []
+  Done.delete_all
   redirect to('/')
 end
