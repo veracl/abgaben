6a26e15692ac79670cd6a237aa78231abed35e98
veracl <veraclemens7@googlemail.com>
2016-06-07 19:48:16 +0200

    Changed indenting from tabs to spaces

--- todoliste/sinatra.rb
+++ todoliste/sinatra.rb
@@ -4,25 +4,25 @@ todos = ["Einkaufen", "Putzen"]
 done = []
 
 get '/' do
-	haml :index, :locals => {:todos => todos, :done => done}
+  haml :index, :locals => {:todos => todos, :done => done}
 end
 
 post '/delete' do
-	todo = params[:todo]
-	todos.delete(todo)
-	done.push(todo)
-	redirect to('/')
+  todo = params[:todo]
+  todos.delete(todo)
+  done.push(todo)
+  redirect to('/')
 end
 
 post '/new' do
-	newtodo = params[:newtodo]
-	if !(newtodo.length == 0)
-		todos.push(newtodo)
-	end
-	redirect to('/')
+  newtodo = params[:newtodo]
+  if !(newtodo.length == 0)
+    todos.push(newtodo)
+  end
+  redirect to('/')
 end
 
 get '/empty' do
-	done = []
-	redirect to('/')
+  done = []
+  redirect to('/')
 end
